7807b63ca5262e6391d9c62736b472b256e77e7f
shreya171899 <shreayb0299@gmail.com>
2023-05-02 21:43:03 +0530

    added css file and styled

--- src/App.js
+++ src/App.js
@@ -1,41 +1,54 @@
+import './categories.scss';
+
 const App=()=> {
+
+  const categories = [
+    {
+      "id": 1,
+      "title": "hats",
+      "imageUrl": 'https://i.ibb.co/cvpntL1/hats.png'
+    },
+    {
+      "id": 2,
+      "title": "jackets",
+      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+    },
+    {
+      "id": 3,
+      "title": "sneakers",
+      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+    },
+    {
+      "id": 4,
+      "title": "womens",
+      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+    },
+    {
+      "id": 5,
+      "title": "mens",
+      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+    }
+  ]
+
+
   return (
     <div className="categories-container">
 
-      <div className="category-container">
-        <div className="category-body">
-          <h2>Hats</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
+      {categories.map(({title,imageUrl,id})=>(
 
-      <div className="category-container">
+      <div className="category-container" key={id}>
         <div className="category-body">
-          <h2>Jackets</h2>
+          <div className="background-image" 
+          style={{
+            backgroundImage: `url(${imageUrl})`
+            }}
+            />
+          <h2>{title.title}</h2>
           <p>Shop Now</p>
         </div>
       </div>
+      ))}
 
-      <div className="category-container">
-        <div className="category-body">
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-
-      <div className="category-container">
-        <div className="category-body">
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-
-      <div className="category-container">
-        <div className="category-body">
-          <h2>Mens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
     </div>
 
   );
